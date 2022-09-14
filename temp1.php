6464fadff4fe9a4c1d7d7ece3c8166b89666119b
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-14 17:49:35 +0300

    Add ." "

--- temp1.php
+++ temp1.php
@@ -1,6 +1,6 @@
 <?php
 $x = rand(-10, 10);
-    echo "<span span style = \" color: purple;\">температура:. </span>". "" . $x; 
+    echo "<span span style = \" color: purple;\">температура:. </span>". $x." "; 
 
 if ($x < 0){
     echo "мороз";
